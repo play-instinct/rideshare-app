e2dbd4b0d54f26e792abc4d75c0e19e76d67146d
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2018-03-21 19:54:36 -0700

    node/mongoose shell

---
+++ models.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {type: String, required: true},
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
